ba898aaaa9419c76246e0fd6b5e007dd70dc9f83
shravan638 <guravn593@gmail.com>
2025-05-20 02:06:25 -0500

    Add files via upload

---
+++ jay.py
@@ -0,0 +1,107 @@
+#first program
+print("Hello world")
+
+#second program
+name="Shravan"
+age=14
+price=27.23
+
+print(name)
+print(age)
+print(price)
+
+#third program
+name="Shravan"
+age=14
+price=27.23
+young=True
+a=None
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(young))
+print(type(a))
+
+#fourth program
+a=34
+b=89
+sum=a+b
+
+print(sum)
+
+a=45
+b=88
+diff=a-b
+
+print(diff)
+
+#for explanig the program we use comment for single line we use # for multi line we use
+"""
+hi my name is
+shravan
+"""
+#fifth program
+a=23
+b=45
+
+print(a+b)
+a=23
+b=45
+
+print(a-b)
+a=23
+b=45
+
+print(a/b)#for divition
+a=23
+b=45
+
+print(a%b)#for finding remiander
+a=23
+b=45
+
+print(a*b)#for multiplaycation
+a=23
+b=45
+
+print(a^b)#for finding power
+a=23
+b=45
+
+print(a==b)
+a=23
+b=45
+
+print(a!=b)
+
+num=10
+num+=10
+
+print("num:",num)
+num=10
+num-=10
+
+print("num:",num)
+num=10
+num*=10
+
+print("num:",num)
+num=10
+num/=5
+
+print("num:",num)
+num=10
+num%=5
+
+print("num:",num)
+num=10
+num**=5
+
+print("num:",num)
+
+#sixth program
+first=int(input("enter first:"))
+second=int(input("enter second:"))
+
+print("sum=",first + second)
